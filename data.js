2590c17785da471726ba1c9924fa262a481af2a5
DavidMoffett <david@moffett.co.nz>
2026-04-26 15:25:11 +1200

    Updated base price to

---
+++ data.js
@@ -0,0 +1,7 @@
+Set-Content data.js 'window.KLICKPIX_CONFIG = {
+    SUPABASE_URL: "https://vzyuoxmshvkgvovjebus.supabase.co",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6eXVveG1zaHZrZ3ZvdmplYnVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEyNjAyNjQsImV4cCI6MjAyNjgzNjI2NH0.7_9n_L0K_z8Q_v4_p3_G_v8_Q_v4_p3_G_v8",
+    PAYPAL_CLIENT_ID: "Ad_2vFmXz5S0vHw2mQ9E_B3sQv6lY_p7-R3U8zX_G_W_C1_D",
+    BASE_PRICE: 7.00
+};'
+
